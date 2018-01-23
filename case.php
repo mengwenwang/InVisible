2abf1898d2c73c80b91e37b39617088cd7b92ff5
mengwenwang <mengwenwang@users.noreply.github.com>
2018-01-23 00:12:23 -0500

    Add files via upload

---
+++ case.php
@@ -0,0 +1,120 @@
+<?php include './partials/header.php' ?>
+<?php include './partials/nav.php' ?>
+
+<!-- 1 -->
+<div class="caseSection">
+    <div class="caseOverlay">
+    </div>
+
+    <div class="caseBanner" style="background-image:url('http://cashmereagency.com/content/uploads/2017/06/atlanta-banner.jpg')"> 
+        <div class="caseBannerContainer">
+            <div class="casePortrait">
+                <img src="http://cashmereagency.com/content/uploads/2017/06/atlanta-thumbnails-300x300.jpg" alt="">
+            </div>
+            <div class="caseTitle">
+                <h2>FX – Atlanta</h2>
+                <h4><i>Atlanta</i> was a highly anticipated show thanks to the large existing fan base of both Donald Glover and his rap pseudonym, Childish Gambino.</h4>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<!-- 2 -->
+<div class="caseCarousel">
+    <div class="caseCarouselContent">
+
+        <!-- left part -->
+        <div class="caseCaroText">
+            <h6>
+             <i>Atlanta </i> was a highly anticipated show thanks to the large existing fan base of both Donald Glover and his rap pseudonym, Childish Gambino. To boost the show’s presence and highlight its dreamlike and hyper-realistic quality, we developed a series of creative imagery and philosophical copy for the show’s official social media platforms, along with an experiential extension in Atlanta that engaged the core audience. The creative concepts were strategically implemented to generate buzz in the weeks leading up to the premiere and to encourage audience tune-in and engagement during the season.
+            </h6>
+            <ul>
+                <li>Starting at approx. 4K Twitter following to 70K+ and growing</li>
+                <li>Compelling Twitter musing hints at the series’ existential vibes</li>
+                <li>@AtlantaFX Twitter’s Innovative Social Marketing</li>
+                <li>Starlight Drive-in Screening of Atlanta</li>
+            </ul>
+        </div>
+
+        <!-- right part -->
+        <div class="caseCarouselSection">
+            <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img class="d-block w-100" src="http://cashmereagency.com/content/uploads/2017/06/Copy-of-HeadphonesV1_01.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100" src="http://cashmereagency.com/content/uploads/2017/06/Copy-of-EP108_Duality_BrownvsRose.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100" src="http://cashmereagency.com/content/uploads/2017/06/FX_ATL_CustomImg_SuperBowl_JY_002.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+                </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- 3 -->
+<div class="casePortfolio">
+    <div class="casePortfolioContent">
+
+        <div class="casePortTitle">
+            <h2>SEE MORE</h2>
+        </div>
+
+        <div class="casePortImgContainer">
+            <div class="imgContainerItem">
+                <div class="caseImgOverlay">
+                </div>
+                <div class="casePortImg">
+                    <img src="http://cashmereagency.com/content/uploads/2017/06/oitnb-thumbnail-600x400.jpg" alt="">
+                </div>
+            </div>  
+            <div class="imgContainerItem">
+                <div class="caseImgOverlay">
+                </div>
+                <div class="casePortImg">
+                    <img src="http://cashmereagency.com/content/uploads/2017/06/oitnb-thumbnail-600x400.jpg" alt="">
+                </div>
+            </div>
+            <div class="imgContainerItem">
+                <div class="caseImgOverlay">
+                </div>
+                <div class="casePortImg">
+                    <img src="http://cashmereagency.com/content/uploads/2017/06/oitnb-thumbnail-600x400.jpg" alt="">
+                </div>
+            </div>
+            
+
+            
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+<?php include './partials/footer.php' ?>
